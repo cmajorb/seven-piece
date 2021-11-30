2f395c57121df021f0d781f3379be5ead682d3ce
cmajorb <cmajorb@gmail.com>
2021-11-29 20:49:07 -0700

    Added canvas

---
+++ app/static/scripts/index.js
@@ -0,0 +1,11 @@
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
+var width = canvas.getAttribute('width');
+var height = canvas.getAttribute('height');
+
+var bgImage = new Image();
+bgImage.src = "static/images/background.jpeg";
+
+bgImage.onload = function(){
+    context.drawImage(bgImage, 0, 0, width/2, height);
+};
