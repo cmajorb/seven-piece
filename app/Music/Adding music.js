2244061f7e21c6a9b03d04df5a98e8bf0ed4fb7c
Kingandred <andrew.hart198@gmail.com>
2021-12-17 15:01:47 -0700

    adding music

--- app/Music/Adding music.js
+++ app/Music/Adding music.js
@@ -1,4 +1,4 @@
-<audio src="Loading Screen.mp3"></audio>
+<audio src="../Music/Loading Screen.mp3"></audio>
 window.addEventListener("DOMContentLoaded", event => {
   const audio = document.querySelector("audio");
   audio.volume = 0.2;
