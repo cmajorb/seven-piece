0bd2ab00671827af4779071ee864a175b87b1131
Kingandred <andrew.hart198@gmail.com>
2021-12-18 16:02:41 -0700

    adding music

--- app/static/scripts/Adding music.js
+++ app/static/scripts/Adding music.js
@@ -1,6 +1,3 @@
-<audio src='../Music/Loading Screen.mp3'></audio>
-const audioContext = new AudioContext();
-const element = document.querySelector(audio);
-const source = audioContext.createMediaElementSource(element);
-source.connect(audioContext.destination)
-audio.play();
+const audioObj = new Audio("../Music/Loading Screen.mp3");
+
+audioObj.play();
