23dbf5ae3ca44e1e0a7a00a74a86daf994d25e36
Kingandred <andrew.hart198@gmail.com>
2021-12-20 16:51:25 -0700

    adding music

--- app/Music/music.py
+++ app/Music/music.py
@@ -1,3 +1,3 @@
 import pygame
-pygame.mixer.music.load("Loading Screen.mp3")
+pygame.mixer.music.load("Users\Hart\documents\seven-piece\app\Music Files\Loading Screen.mp3")
 pygame.mixer.music.play(-1, 0.0)
