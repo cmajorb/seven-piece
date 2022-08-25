469ea8ffbd18787875e0d2b75863c848a7d04ac5
Major Brown <majorbrown@Majors-MacBook-Pro.local>
2022-08-24 20:52:08 -0700

    fixed static styling

--- django/sevenpiece/sevenpiece/settings.py
+++ django/sevenpiece/sevenpiece/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
 env = environ.Env()
 environ.Env.read_env()
@@ -124,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
