c77b9c199242f308574543a3c45e6711a7dd6262
Major Brown <cmajorb@gmail.com>
2022-08-26 06:48:15 -0700

    Added map and colorscheme models

--- django/sevenpiece/game/admin.py
+++ django/sevenpiece/game/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from game.models import Map, ColorScheme
 # Register your models here.
+
+admin.site.register(Map)
+admin.site.register(ColorScheme)
--- django/sevenpiece/game/models.py
+++ django/sevenpiece/game/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class ColorScheme(models.Model):
+    name = models.CharField(max_length=150, null=False, unique=False)
+    scheme = models.JSONField()
+    max_player_size = models.IntegerField()
+    def __str__(self):
+        return self.name + "(" + self.max_player_size + ") "
+
+class Map(models.Model):
+    name = models.CharField(max_length=150, null=False, unique=False)
+    data = models.JSONField()
+    player_size = models.IntegerField()
+    color_scheme = models.ForeignKey(ColorScheme, on_delete=models.SET_NULL, null=True)
+    def __str__(self):
+        return self.name + "(" + self.player_size + ") "
