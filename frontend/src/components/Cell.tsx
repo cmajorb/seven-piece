c1c66ebb83c41db31fbe10154c9901c4d2782ebe
NathanielHart44 <nathaniel.hart@gmail.com>
2022-09-10 20:22:31 -0600

    updated parent/child cell/grid rendering

--- frontend/src/components/Cell.tsx
+++ frontend/src/components/Cell.tsx
@@ -6,22 +6,17 @@ import { useState, useEffect } from 'react';
 
 type Props = {
   location: number[],
-  gameState: any[]
+  gameState: any[],
+  selected: boolean,
+  updateSelectedTile: any,
 };
 
 // ----------------------------------------------------------------------
 
-export default function Cell({ location, gameState }: Props) {
+export default function Cell({ location, gameState, selected, updateSelectedTile }: Props) {
   
-  const constants_info = require('../testing/constants.json');
   const pieces_info = require('../testing/pieces.json');
   const theme = useTheme();
-
-  const [selected, setSelected] = useState<boolean>(false);
-  const handleSelected = () => {
-    if (selected) { setSelected(false) }
-    else (setSelected(true));
-  };
 
   const getCharacterImage = (location: number[], gameState: any[], pieces_info: any) => {
     const current_location = gameState[location[0]][location[1]];
@@ -37,17 +32,8 @@ export default function Cell({ location, gameState }: Props) {
   }
   const fill_image_url: string | undefined = getCharacterImage(location, gameState, pieces_info);
 
-  const getCellStatus = (location: number[], gameState: any[], constants_info: any) => {
+  const getCellStatus = (location: number[], gameState: any[]) => {
     const current_location = gameState[location[0]][location[1]];
-    // TESTING VISIBILITY
-    // const constants_keys = Object.keys(constants_info);
-    // for (let index in constants_keys) {
-    //   const key = constants_keys[index];
-    //   if (constants_info[key as keyof typeof constants_info] === current_location) {
-    //     console.log(`LOCATION: [${location}], CELL STATUS: ${key}, STATUS NUMBER: ${current_location}`);
-    //     break;
-    //   }
-    // };
     return current_location;
   }
 
@@ -58,10 +44,11 @@ export default function Cell({ location, gameState }: Props) {
       sx={{ minWidth: 50, minHeight: 50, border: 2,
         borderColor: selected? theme.palette.primary.main : theme.palette.common.black,
         ...(selected && { bgcolor: selected && theme.palette.grey[200] }),
-        ...(getCellStatus(location, gameState, constants_info) === 4 && { backgroundColor: theme.palette.common.black }),
+        ...(getCellStatus(location, gameState) === 4 && { backgroundColor: theme.palette.common.black }),
         '&:hover': { bgcolor: theme.palette.grey[200], cursor: 'pointer' },
       }}
-      onClick={() => { getCellStatus(location, gameState, constants_info); handleSelected() }}
+      // onClick={() => { getCellStatus(location, gameState, constants_info); handleSelected() }}
+      onClick={() => { getCellStatus(location, gameState); updateSelectedTile(location) }}
     >
       { fill_image_url && <img width={45} height={45} alt='test' src={fill_image_url}/> }
     </Card>
--- frontend/src/components/MainGrid.tsx
+++ frontend/src/components/MainGrid.tsx
@@ -1,5 +1,6 @@
 import { Stack } from '@mui/material';
 import Cell from './Cell';
+import { useState, useEffect } from 'react';
 
 // ----------------------------------------------------------------------
 
@@ -13,24 +14,53 @@ type Props = {
 
 export default function MainGrid({ rows, columns, board }: Props) {
   
+    const constants_info = require('../testing/constants.json');
+
     const column_nums = Array.from(Array(columns).keys());
     const row_nums = Array.from(Array(rows).keys());
+    const [selectedTile, setSelectedTile] = useState<number[]>([]);
+
+    useEffect(() => {}, [selectedTile])
+
+    const printCellStatus = (location: number[], gameState: any[], constants_info: any) => {
+        const current_location = gameState[location[0]][location[1]];
+        const constants_keys = Object.keys(constants_info);
+        for (let index in constants_keys) {
+          const key = constants_keys[index];
+          if (constants_info[key as keyof typeof constants_info] === current_location) {
+            console.log(`LOCATION: [${location}], CELL STATUS: ${key}, STATUS NUMBER: ${current_location}`);
+            break;
+          }
+        };
+      }
+
+    const updateSelectedTile = (location: number[]) => {
+        console.log(`SELECTED: [${selectedTile[0]},${selectedTile[1]}]`, `CLICKED: [${location[0]},${location[1]}]`);
+        if ((location[0] !== selectedTile[0]) || (location[1] !== selectedTile[1])) { setSelectedTile(location) }
+        else if ((location[0] === selectedTile[0]) && (location[1] === selectedTile[1])) { setSelectedTile([]) };
+        printCellStatus(location, board, constants_info);
+    }
+
+    const calcSelectedTile = (selected_tile: number[], current_tile: number[]) => {
+        if ((selected_tile[0] === current_tile[0]) && (selected_tile[1] === current_tile[1])) { return true }
+        else { return false }
+    }
 
     // TEST FUNCTION
-    const printRows = (board: any[]) => {
-        for (let index in board) {
-            const row = board[index];
-            console.log("ROW", index, row);
-          };
-    }
-    printRows(board);
+    // const printRows = (board: any[]) => {
+    //     for (let index in board) {
+    //         const row = board[index];
+    //         console.log("ROW", index, row);
+    //       };
+    // }
+    // printRows(board);
 
     return (
         <Stack spacing={0.25} direction={'row'}>
             {column_nums.map((column) => (
                 <Stack spacing={0.25} direction={'column'}>
                     {row_nums.map((row) => (
-                        <Cell location={[row, column]} gameState={board}/>
+                        <Cell location={[row, column]} gameState={board} selected={calcSelectedTile(selectedTile, [row, column])} updateSelectedTile={updateSelectedTile}/>
                     ))}
                 </Stack>
             ))}
