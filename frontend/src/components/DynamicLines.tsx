8f899130d3bc4bb3000d42871dc63c43aac6a078
NathanielHart44 <nathaniel.hart@gmail.com>
2022-09-22 02:32:53 -0600

    added gradient

--- frontend/src/components/DynamicLines.tsx
+++ frontend/src/components/DynamicLines.tsx
@@ -1,5 +1,6 @@
 import { Stack, Divider } from '@mui/material';
 import { keyframes } from '@mui/system';
+import LineBackground from '../images/platinum_texture.jpeg';
 
 // ----------------------------------------------------------------------
 
@@ -18,9 +19,9 @@ const line_grow = (
     to {
         width:100%;
     }`
-  );
+);
 
-  const line_fade = (
+const line_fade = (
     keyframes`
     from {
         width: 100%;
@@ -28,15 +29,23 @@ const line_grow = (
     to {
         width:0%;
     }`
-  );
+);
 
 export function TurnLine({ is_turn, bg_color }: TurnLineProps) {
+
+    const background_img: string = 'linear-gradient(to top, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0) 50%, #69ACEF 75%, #3064F2 100%), linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 0) 50%, #69ACEF 75%, #3064F2 100%)';
+
     return (
         <Stack justifyContent={'center'} alignItems={'center'}>
-            { is_turn ?
-            <Divider variant="middle" color={bg_color} sx={{ margin: '0 20px', borderRadius: '10px', height: 4, animation: `${line_grow} 2s forwards ease-in-out` }}/> :
-            <Divider variant="middle" color={bg_color} sx={{ margin: '0 20px', borderRadius: '10px', borderColor: bg_color, height: 4, animation: `${line_fade} 2s forwards ease-in-out` }}/> }
-
+            { is_turn ?            
+            <Divider
+                variant="middle"
+                sx={{ margin: '0 20px', borderRadius: '10px', height: 8, animation: `${line_grow} 2s forwards ease-in-out`, backgroundImage: background_img }}
+            /> :
+            <Divider
+                variant="middle"
+                sx={{ margin: '0 20px', borderRadius: '10px', height: 8, animation: `${line_fade} 2s forwards ease-in-out`, backgroundImage: background_img }}
+            /> }          
         </Stack>
     );
 }
