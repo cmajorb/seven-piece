183a70369f1430c26b4f0f76d3afdfdcd5385dea
NathanielHart44 <nathaniel.hart@gmail.com>
2022-09-22 02:34:42 -0600

    changed bottom bar parent

--- frontend/src/components/MainGrid.tsx
+++ frontend/src/components/MainGrid.tsx
@@ -6,6 +6,7 @@ import { Piece, Map, PieceActions } from '../types';
 import calcSelectedTile from '../utils/calcSelectedTile';
 import checkSameLocation from '../utils/checkSameLocation';
 import getCellStatus from '../utils/getCellStatus';
+import PieceBottomBar from './PieceBottomBar';
 
 // ----------------------------------------------------------------------
 
@@ -14,7 +15,7 @@ type Props = {
     columns: number,
     pieces: Piece[],
     map: Map,
-    active_turn: boolean,
+    active_player_id: number | undefined,
     this_player_id: number,
     objectives: string[],
     submitPieceAction: any
@@ -22,9 +23,10 @@ type Props = {
 
 // ----------------------------------------------------------------------
 
-export default function MainGrid({ rows, columns, pieces, map, active_turn, objectives, this_player_id, submitPieceAction }: Props) {
+export default function MainGrid({ rows, columns, pieces, map, active_player_id, objectives, this_player_id, submitPieceAction }: Props) {
     
     const theme = useTheme();
+    const active_turn: boolean = (active_player_id && active_player_id === this_player_id) ? true : false;
 
     const column_nums = (Array.from(Array(columns).keys()));
     const row_nums = (Array.from(Array(rows).keys())).sort((a, b) => b - a);
@@ -35,11 +37,11 @@ export default function MainGrid({ rows, columns, pieces, map, active_turn, obje
     useEffect(() => {}, [selectedTile]);
     useEffect(() => { if (selectedPiece) { console.log("Currently Selected Piece:", selectedPiece) } }, [selectedPiece]);
 
-    const updateSelected = (location: number[], piece: Piece | undefined) => {
+    const updateSelected = (location: number[], piece: Piece | undefined, checking: boolean) => {
         const same_location = checkSameLocation(location, selectedTile);
         if (same_location) { setSelectedTile([]); setSelectedPiece(undefined) }
         else {
-            if (selectedPiece) { submitPieceAction(selectedPiece.id, location, actionType) };
+            if (selectedPiece && !checking) { submitPieceAction(selectedPiece.id, location, actionType) };
             setSelectedTile(location);
             if (piece && piece.player === this_player_id) { setSelectedPiece(piece) }
             else if (piece && piece.player !== this_player_id) { console.log("CLICKED ON ANOTHER PIECE") }
@@ -50,41 +52,55 @@ export default function MainGrid({ rows, columns, pieces, map, active_turn, obje
     const handleActionType = (action_type: PieceActions) => { setActionType(action_type) };
 
     return (
-        <Stack spacing={2}>
-            <Stack direction={'row'} spacing={2} alignContent={'center'} justifyContent={'space-around'}>
-                <Paper
-                    sx={{ p: 1, backgroundImage: `url("https://d36mxiodymuqjm.cloudfront.net/website/battle/backgrounds/bg_stone-floor.png")` }}
-                    elevation={2}
-                    onContextMenu={(e)=> e.preventDefault()}
-                    onMouseDown={(e)=> e.preventDefault()}
-                >
-                    <Stack spacing={0.25} direction={'row'}>
-                        {column_nums.map((column) => (
-                            <Stack key={column} spacing={0.25} direction={'column'}>
-                                {row_nums.map((row) => (
-                                    <Cell key={([row, column]).toString()}
-                                        location={[row, column]}
-                                        selected={calcSelectedTile(selectedTile, [row, column])}
-                                        cell_status={getCellStatus(objectives, map.data, [row, column])}
-                                        pieces={pieces}
-                                        color_scheme={map.color_scheme}
-                                        this_player_id={this_player_id}
-                                        updateSelected={updateSelected}
-                                    />
-                                ))}
-                            </Stack>
-                        ))}
-                    </Stack>
-                </Paper>
-                <Divider orientation="vertical" flexItem color={theme.palette.common.black} />
-                <PieceDetails
-                    selected_piece={selectedPiece}
+        <>
+            <Stack spacing={2}>
+                <Stack direction={'row'} spacing={2} alignContent={'center'} justifyContent={'space-around'}>
+                    <Paper
+                        sx={{ p: 1, backgroundImage: `url("https://d36mxiodymuqjm.cloudfront.net/website/battle/backgrounds/bg_stone-floor.png")` }}
+                        elevation={2}
+                        onContextMenu={(e)=> e.preventDefault()}
+                        onMouseDown={(e)=> e.preventDefault()}
+                    >
+                        <Stack spacing={0.25} direction={'row'}>
+                            {column_nums.map((column) => (
+                                <Stack key={column} spacing={0.25} direction={'column'}>
+                                    {row_nums.map((row) => (
+                                        <Cell key={([row, column]).toString()}
+                                            location={[row, column]}
+                                            selected={calcSelectedTile(selectedTile, [row, column])}
+                                            cell_status={getCellStatus(objectives, map.data, [row, column])}
+                                            pieces={pieces}
+                                            color_scheme={map.color_scheme}
+                                            this_player_id={this_player_id}
+                                            updateSelected={updateSelected}
+                                        />
+                                    ))}
+                                </Stack>
+                            ))}
+                        </Stack>
+                    </Paper>
+                    <Divider orientation="vertical" flexItem color={theme.palette.common.black} />
+                    <PieceDetails
+                        selected_piece={selectedPiece}
+                        selected_action={actionType}
+                        this_player_id={this_player_id}
+                        active_turn={active_turn}
+                        handleActionType={handleActionType}
+                    />
+                </Stack>
+            </Stack>
+            { pieces && (this_player_id !== undefined) &&
+                <PieceBottomBar
+                    pieces={pieces}
+                    selected_tile={selectedTile}
                     selected_action={actionType}
                     this_player_id={this_player_id}
-                    active_turn={active_turn}
+                    color_scheme={map.color_scheme}
+                    active_player_id={active_player_id}
+                    updateSelected={updateSelected}
                     handleActionType={handleActionType}
                 />
-            </Stack>
-        </Stack>
+            }
+        </>
     );
 }
