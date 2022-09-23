a754f172708cf71a371cf13361c02a3739027ec0
Major Brown <cmajorb@gmail.com>
2022-09-22 17:20:54 -0700

    Added id to map serializer

--- django/sevenpiece/game/serializers.py
+++ django/sevenpiece/game/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 class MapSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
     name = serializers.CharField(max_length=150)
     player_size = serializers.IntegerField()
     num_characters = serializers.IntegerField()
