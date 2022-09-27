6f4e511e5ce7fdf7a13119fd62d255eb766350f0
NathanielHart44 <nathaniel.hart@gmail.com>
2022-09-27 17:55:13 -0600

    turn arrow change

--- frontend/src/components/bottom-bar/B-BarActionArea.tsx
+++ frontend/src/components/bottom-bar/B-BarActionArea.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Button } from '@mui/material';
+import { Box, Stack, Button, useTheme } from '@mui/material';
 import { ColorScheme } from '../../types';
 import GetBorderColor from '../../utils/getBorderColor';
 import Iconify from '../misc/Iconify';
@@ -18,47 +18,49 @@ type Props = {
 
 export default function BBarActionArea({ this_player_id, color_scheme, active_player_id, current_state, endTurn, setPieces }: Props) {
 
-  return (
-    <>
-        { (active_player_id !== undefined && active_player_id === 1 && current_state === 'PLAYING') ?
-            <Box sx={{ width: 70, height: 70 }}/> :
-            <>
-            { active_player_id !== undefined && current_state === 'PLAYING' &&
-            <Box sx={{ width: 70, height: 70 }}>
-                <Iconify
-                icon={'eva:arrowhead-left-outline'}
-                width={70}
-                height={70}
-                color={GetBorderColor(color_scheme, active_player_id, true)}
-                />
-            </Box> }
-            </> 
-        }
+    const theme = useTheme();
 
-        <Stack spacing={1} justifyContent={'center'} alignItems={'center'}>
-            { current_state === 'PLACING' &&
-            <Button fullWidth variant={'contained'} onClick={() => { setPieces(JSON.stringify(["Soldier", "Berserker"])) }}>
-                Place Pieces
-            </Button> }
-            <Button fullWidth variant={'contained'} onClick={() => { endTurn() }} disabled={(this_player_id !== active_player_id)}>
-                End Turn
-            </Button>
-        </Stack>
+    return (
+        <>
+            { (active_player_id !== undefined && active_player_id === 1 && current_state === 'PLAYING') ?
+                <Box sx={{ width: 60, height: 60 }}/> :
+                <>
+                { active_player_id !== undefined && current_state === 'PLAYING' &&
+                <Box sx={{ width: 60, height: 60, backgroundColor: 'black', border: 2, borderRadius: '16px', borderColor: theme.palette.grey[700] }}>
+                    <Iconify
+                    icon={'eva:arrowhead-left-outline'}
+                    width={60}
+                    height={60}
+                    color={GetBorderColor(color_scheme, active_player_id, true)}
+                    />
+                </Box> }
+                </> 
+            }
 
-        { (active_player_id !== undefined && active_player_id === 0 && current_state === 'PLAYING') ?
-            <Box sx={{ width: 70, height: 70 }}/> :
-            <>
-            { active_player_id !== undefined && current_state === 'PLAYING' &&
-            <Box sx={{ width: 70, height: 70 }}>
-                <Iconify
-                icon={'eva:arrowhead-right-outline'}
-                width={70}
-                height={70}
-                color={GetBorderColor(color_scheme, active_player_id, true)}
-                />
-            </Box> }
-            </> 
-        }
-    </>
-  );
+            <Stack spacing={1} justifyContent={'center'} alignItems={'center'}>
+                { current_state === 'PLACING' &&
+                <Button fullWidth variant={'contained'} onClick={() => { setPieces(JSON.stringify(["Soldier", "Berserker"])) }}>
+                    Place Pieces
+                </Button> }
+                <Button fullWidth variant={'contained'} onClick={() => { endTurn() }} disabled={(this_player_id !== active_player_id)}>
+                    End Turn
+                </Button>
+            </Stack>
+
+            { (active_player_id !== undefined && active_player_id === 0 && current_state === 'PLAYING') ?
+                <Box sx={{ width: 60, height: 60 }}/> :
+                <>
+                { active_player_id !== undefined && current_state === 'PLAYING' &&
+                <Box sx={{ width: 60, height: 60, backgroundColor: 'black', border: 2, borderRadius: '16px', borderColor: theme.palette.grey[700] }}>
+                    <Iconify
+                    icon={'eva:arrowhead-right-outline'}
+                    width={60}
+                    height={60}
+                    color={GetBorderColor(color_scheme, active_player_id, true)}
+                    />
+                </Box> }
+                </> 
+            }
+        </>
+    );
 }
