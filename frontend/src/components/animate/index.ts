fa3f955c360e2ed85b708861ff1982e5385995ee
NathanielHart44 <nathaniel.hart@gmail.com>
2022-09-30 01:33:04 -0600

    framer motion

---
+++ frontend/src/components/animate/index.ts
@@ -0,0 +1,12 @@
+// ----------------------------------------------------------------------
+
+export * from './variants';
+
+export { default as DialogAnimate } from './DialogAnimate';
+export { default as TextAnimate } from './TextAnimate';
+
+export { default as FabButtonAnimate } from './FabButtonAnimate';
+export { default as IconButtonAnimate } from './IconButtonAnimate';
+
+export { default as MotionContainer } from './MotionContainer';
+export { default as MotionLazyContainer } from './MotionLazyContainer';
